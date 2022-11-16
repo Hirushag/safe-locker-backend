4079ff3940ef8fd6ed08f1023e653e5774bb95c6
Gagana Hirusha <it19034478@my.sliit.lk>
2022-11-17 01:14:17 +0530

    Initialize Backend

---
+++ src/main/java/com/sliit/safelocker/SafelockerApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.safelocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafelockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafelockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sliit/safelocker/SafelockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.safelocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafelockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
